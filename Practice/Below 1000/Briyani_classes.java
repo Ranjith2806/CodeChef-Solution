3383a568ed45d1e8749813f126c9677cf9be54ee
Ranjithkumar S <105041389+Ranjith2806@users.noreply.github.com>
2022-08-09 23:03:33 +0530

    BRIYANI-PRACTICE
    https://www.codechef.com/problems/BIRYANI

---
+++ Practice/Below 1000/Briyani_classes.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        int test =sc.nextInt();
+        while(test-->0)
+        {
+            int X = sc.nextInt();
+            int Y = sc.nextInt();
+            System.out.println(X * Y);
+        }
+        
+    }
+}
