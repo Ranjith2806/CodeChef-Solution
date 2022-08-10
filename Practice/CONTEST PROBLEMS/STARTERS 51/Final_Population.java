2014660e06f1a9f94a6803d959fa99cb1bc1981c
Ranjithkumar S <105041389+Ranjith2806@users.noreply.github.com>
2022-08-10 23:18:11 +0530

    POPULATION - START51
    https://www.codechef.com/START51D/problems/POPULATION?tab=statement

---
+++ Practice/CONTEST PROBLEMS/STARTERS 51/Final_Population.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        while(test-->0)
+        {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int z = sc.nextInt();
+            System.out.println((x - y)+ z);
+        }
+    }
+}
