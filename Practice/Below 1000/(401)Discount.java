d2470c851d5c7e34763dfefd76ed70a135a348de
Ranjithkumar S <105041389+Ranjith2806@users.noreply.github.com>
2022-08-12 23:48:55 +0530

    DISCNT - START50
    https://www.codechef.com/submit/DISCNT

---
+++ Practice/Below 1000/(401)Discount.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class Codechef
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        while(test-->0)
+        {
+            int x = sc.nextInt();
+            System.out.println(100 - x);
+        }
+    }
+}
