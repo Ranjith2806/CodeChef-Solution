223caee8d98e894f325fe8d8c9aaec3b0abe1ea3
Ranjithkumar S <105041389+Ranjith2806@users.noreply.github.com>
2022-08-13 00:02:57 +0530

    CCHOCOLATES - START41
    https://www.codechef.com/submit/CCHOCOLATES?tab=statement

--- Practice/Below 1000/(405)Chef_and_Chocolate.java
+++ Practice/Below 1000/(405)Chef_and_Chocolate.java
@@ -1,23 +1,21 @@
-/* package codechef; // don't place package name! */
-
 import java.util.*;
-import java.lang.*;
 import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
+import java.lang.*;
 class Codechef
 {
-	public static void main (String[] args) throws java.lang.Exception
-	{
-	    Scanner input=new Scanner(System.in);
-	    int testcase=input.nextInt();
-	    while(testcase>0){
-	        int x=(input.nextInt())*5;
-	        int y=(input.nextInt())*10;
-	        int totalchoclate=input.nextInt();
-	        int calculation=((x+y)/totalchoclate);
-	        System.out.println(calculation);
-	        testcase--;
-	    }
-	}
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        while(test-->0)
+        {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int z = sc.nextInt();
+            int a = ((x * 5) + (y * 10));
+            int b = (a/z);
+            System.out.println(b);
+            
+        }
+    }
 }
