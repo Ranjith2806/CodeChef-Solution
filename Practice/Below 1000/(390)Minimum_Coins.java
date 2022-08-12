66b89177c55ba0e61e9059bf454c29376c11cbee
Ranjithkumar S <105041389+Ranjith2806@users.noreply.github.com>
2022-08-12 23:47:07 +0530

    MINCOINSREQ - START47
    https://www.codechef.com/submit/MINCOINSREQ?tab=submissions

---
+++ Practice/Below 1000/(390)Minimum_Coins.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        while(test-->0)
+        {
+            int x = sc.nextInt();
+            System.out.println(x %10);
+        }
+    }
+}
