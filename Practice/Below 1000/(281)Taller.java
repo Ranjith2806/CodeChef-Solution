f3d60fcbd43db77decd07ddf34c3333b1a709975
Ranjithkumar S <105041389+Ranjith2806@users.noreply.github.com>
2022-08-14 22:30:48 +0530

    TALLER - PRACTICE
    https://www.codechef.com/problems/TALLER

---
+++ Practice/Below 1000/(281)Taller.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        while(test-->0)
+        {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            if(A<B)
+            {
+                System.out.println("B");
+            }
+            else System.out.println("A");
+            
+            
+        }
+    }
+}
