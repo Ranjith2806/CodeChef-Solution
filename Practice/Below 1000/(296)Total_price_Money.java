d703faffa9edbc4c6262e34d2e3f677d663a2915
Ranjithkumar S <105041389+Ranjith2806@users.noreply.github.com>
2022-08-14 22:40:31 +0530

    PRIZEPOOL - MAY222
    https://www.codechef.com/submit/PRIZEPOOL?tab=statement

---
+++ Practice/Below 1000/(296)Total_price_Money.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Codechef
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        while(test-->0)
+        {
+            int X = sc.nextInt();
+            int Y = sc.nextInt();
+            // int R = (10*X);
+            // int S = (90*Y);
+            System.out.println((10*X) + (90*Y));
+        }
+    }
+})
