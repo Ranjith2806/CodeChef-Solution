1f12861606c492680b62dfe18e3bb686775db286
Ranjithkumar S <105041389+Ranjith2806@users.noreply.github.com>
2022-08-16 23:28:21 +0530

    FBC - START29
    https://www.codechef.com/submit/FBC

---
+++ Practice/Below 1000/(419)Fill_the_Bucket.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int test = sc.nextInt();
+        while(test-->0)
+        {
+            int k = sc.nextInt();
+            int x = sc.nextInt();
+            System.out.println(k - x);
+        }
+    }
+}
