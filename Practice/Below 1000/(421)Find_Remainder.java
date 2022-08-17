05e266b9573519ba17e7a9ecd7652df3b54d9052
Ranjithkumar S <105041389+Ranjith2806@users.noreply.github.com>
2022-08-17 20:43:33 +0530

    FLOW002
    https://www.codechef.com/submit/FLOW002

---
+++ Practice/Below 1000/(421)Find_Remainder.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+class Codechef{
+public static void main(String[] args) throws IOException{
+ Scanner sc = new Scanner(System.in);
+ int t = sc.nextInt();
+ while(t-->0)
+ {
+ int p = sc.nextInt();
+ int b = sc.nextInt();
+ 
+ System.out.println(p%b);
+ 
+ }
+ }
+ 
+}
