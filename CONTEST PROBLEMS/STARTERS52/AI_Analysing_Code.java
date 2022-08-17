820c6a3babc489882973eca144c09dd14432e546
Ranjithkumar S <105041389+Ranjith2806@users.noreply.github.com>
2022-08-17 21:59:10 +0530

    AIANALYSE - START52
    https://www.codechef.com/START52D/problems/AIANALYSE

---
+++ CONTEST PROBLEMS/STARTERS52/AI_Analysing_Code.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class Codechef
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int c = sc.nextInt();
+        if(c <= 1000)
+        {
+            System.out.println("YES");
+        }
+        else System.out.println("NO");
+    }
+}
