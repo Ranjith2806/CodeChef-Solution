63719a41fd5a3ce107996537fe818de7d1fb792d
Ranjithkumar S <105041389+Ranjith2806@users.noreply.github.com>
2022-08-21 23:32:32 +0530

    SUGARCANE - MAY221
    https://www.codechef.com/submit/SUGARCANE?tab=statement

---
+++ Practice/Below 1000/(563)Sugarcane_Juice_Business.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class Codechef
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+    
+    for(int i=0;i<t;i++)
+    {
+        int n = sc.nextInt();
+        System.out.println(n * 15);
+    }
+    }
+}
