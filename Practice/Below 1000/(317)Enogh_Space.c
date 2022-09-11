382724244dceb047c56cc41099c6834d1658a1f8
Ranjith <105041389+Ranjith2806@users.noreply.github.com>
2022-09-11 23:16:30 +0530

    ENSPACE - COOK143
    Try it Yourself!!
    https://www.codechef.com/submit/ENSPACE?tab=statement

---
+++ Practice/Below 1000/(317)Enogh_Space.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	int t,n,x,y;
+	scanf("%d", &t);
+	for(int i=0;i<t;i++)
+	{
+	    scanf("%d %d %d",&n, &x, &y);
+	    if((x+(2*y))<=n)
+	      printf("YES\n");
+	    else printf("NO\n");
+	}
+	return 0;
+}
