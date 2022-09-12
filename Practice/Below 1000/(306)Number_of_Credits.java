5c21e3f9810ce6a5311dfd75a0216a0bb0451627
Ranjith <105041389+Ranjith2806@users.noreply.github.com>
2022-09-12 23:35:51 +0530

    CREDS - START54
    Try it yourself!!
    https://www.codechef.com/submit/CREDS?tab=statement

---
+++ Practice/Below 1000/(306)Number_of_Credits.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int t = sc.nextInt();
+	    while(t-->0)
+	   // for(int i=0;i<t;i++)
+	    {
+	       int x = sc.nextInt();
+	       int y = sc.nextInt();
+	       int z = sc.nextInt();
+	       System.out.println(((x*4)+(y*2)+(z*0)));
+	    }
+	    
+	
+	}
+}
