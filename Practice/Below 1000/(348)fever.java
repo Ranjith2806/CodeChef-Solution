6aed6b3834beed2bdee7a5269150b7dd94130659
Ranjith <105041389+Ranjith2806@users.noreply.github.com>
2022-09-14 23:29:15 +0530

    FEVER - START55
    Try it Yourself!!
    https://www.codechef.com/submit/FEVER

---
+++ Practice/Below 1000/(348)fever.java
@@ -0,0 +1,44 @@
+// Code in JAVA
+
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+class Codechef
+{
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0)
+        // for(int i=0;i<t;i++)
+        {
+            int x = sc.nextInt();
+            if(x>98)
+            {
+                System.out.println("YES");
+            }
+            else System.out.println("NO");
+        }
+    }
+}
+
+
+// CODE IN C:-
+  
+// #include <stdio.h>
+// int main()
+// {
+//     int t,x;
+//     scanf("%d",&t);
+//     for(int i=0;i<t;i++)
+//     {
+//         scanf("%d",&x);
+//     if(x>98)
+//     {
+//         printf("YES\n");
+//     }
+//     else printf("NO\n");
+//     }
+//     return 0;
+    
+// }
