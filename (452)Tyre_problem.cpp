e45443e6c2a71682571d2d0eb15f7865cf1225a3
Ranjith <105041389+Ranjith2806@users.noreply.github.com>
2022-09-16 23:37:54 +0530

    TYRE - START29
    Try it Yourself!!!
    https://www.codechef.com/submit/TYRE?tab=statement

---
+++ (452)Tyre_problem.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int t,i,n,m;
+    cin>>t;
+    for(i=0;i<t;i++)
+    {
+        cin>>n;
+        cin>>m;
+        cout<<((2*n)+(4*m))<<endl;
+    }
+    return 0;
+}
