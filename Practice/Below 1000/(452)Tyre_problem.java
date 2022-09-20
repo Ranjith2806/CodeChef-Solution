0781da746b6249ccb23db7cb5872426c12b8aa8a
Ranjith <105041389+Ranjith2806@users.noreply.github.com>
2022-09-21 00:06:42 +0530

    TYRE - START29
    https://www.codechef.com/problems/TYRE?tab=statement

---
+++ Practice/Below 1000/(452)Tyre_problem.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class Codechef
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            System.out.println((2*n)+(4*m));
+        }
+    }
+}
