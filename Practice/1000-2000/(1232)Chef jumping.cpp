8b54d173a2dfbefafbb2d96409748a43e4f2fcc4
Ranjith <105041389+Ranjith2806@users.noreply.github.com>
2022-11-24 22:55:34 +0530

    OJUMPS - MAY14
    Try it yourself!!
    https://www.codechef.com/problems/OJUMPS

---
+++ Practice/1000-2000/(1232)Chef jumping.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    long n;
+    cin>>n;
+    if(n%6==3 || n%6==0 || n%6==1)
+    {
+        cout<<"yes";
+    }
+    else
+    cout<<"no";
+    
+}
