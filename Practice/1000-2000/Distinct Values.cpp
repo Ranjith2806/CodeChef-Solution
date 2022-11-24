8dd2cdaed9c072a2f0f8c91b3d9385a98cf423f5
Ranjith <105041389+Ranjith2806@users.noreply.github.com>
2022-11-24 22:57:20 +0530

    DISVAL

---
+++ Practice/1000-2000/Distinct Values.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        cin>>arr[i];
+        int count=0;
+        sort(arr,arr+n);
+        for(int i=0;i<n;i++){
+            if(arr[i]==arr[i+1]){
+                count++;
+                arr[i]=arr[i]+arr[i+1];
+            }
+        }
+        cout<<count<<endl;
+    }
+}
