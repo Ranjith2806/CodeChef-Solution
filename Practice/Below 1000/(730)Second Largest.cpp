fbf590ebeb08e39377ebce3c204a52dba7eb5f86
Ranjith <105041389+Ranjith2806@users.noreply.github.com>
2022-11-26 10:40:09 +0530

    FLOW017
    link for Problem
    https://www.codechef.com/problems/FLOW017

--- Practice/Below 1000/(730)Second Largest.cpp
+++ Practice/Below 1000/(730)Second Largest.cpp
@@ -1,3 +1,4 @@
+//APPROACH-1(By taking the input as array,by sorting the array and printing the second elememt)
 #include<iostream>
 #include<algorithm>
 using namespace std;
@@ -13,5 +14,24 @@ int main(){
         }
         sort(arr,arr+3);
         cout<<arr[1]<<endl;
+    }
+}    //Time Complexity:o(nlogn);
+
+
+//APPROACH-2(by finding maximum and minium of three numbers if a is not max and min then it is required output similarly for b and c)
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        int maxi = max(max(a,b),c);
+        int minm = min(min(a,b),c);
+        cout<<((a!=maxi && a!=minm)? a:(b!=maxi && b!=minm)? b:c)<<endl;
     }
 }
