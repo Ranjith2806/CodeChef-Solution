67da1b0bc36f3ce2991ab2a21f0d73ce443a8900
Ranjith <105041389+Ranjith2806@users.noreply.github.com>
2022-12-03 16:26:52 +0530

    PRB01 - In both C++ and Java lang
    Link for problem
    https://www.codechef.com/problems/PRB01

--- Practice/Below 1000/(794)Primality_test.java
+++ Practice/Below 1000/(794)Primality_test.java
@@ -29,3 +29,56 @@ class Codechef
 		}
 	}
 }
+
+
+In C++ Approach1
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n)
+{
+		if (n <= 1)
+		return false;
+    for (int i = 2; i < n; i++)
+		if (n % i == 0)
+			return false;
+
+    	return true;
+}
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    isPrime(n) ? cout << "yes\n" :cout<<"no\n";
+	}
+}
+*/
+Approach 2 in C++
+/*
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,count=0;
+        cin>>n;
+        for(int i=2;i<=n;i++)
+        {
+            if(n%i==0)
+            count++;
+        }
+        if(count==1)
+        cout<<"yes"<<endl;
+        else
+        cout<<"no"<<endl;
+    }
+}
+*/
