e7e9a4d8c86c5554bf2c73020c2f6e84ff41e5a1
Ranjith <105041389+Ranjith2806@users.noreply.github.com>
2022-12-03 15:51:45 +0530

    DPOLY - MAY222
    Link for Problem
    https://www.codechef.com/problems/DPOLY

--- Practice/Below 1000/(793)Degree_of_polynomial.java
+++ Practice/Below 1000/(793)Degree_of_polynomial.java
@@ -23,3 +23,32 @@ class Codechef
 		}
 	}
 }
+
+
+
+In C++ the solution will be
+/*#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+        int count=0;
+        for(int j=0;j<n;j++)
+        {
+            if(arr[j]!=0)
+            count=j;
+        }
+        cout<<count<<endl;
+    }
+}
+*/
