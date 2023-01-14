0681a9f672161b52c1547ab782f61817eb2ade93
Rankumar <105041389+Ranjith2806@users.noreply.github.com>
2023-01-14 22:05:51 +0530

    Update and rename (794)Primality_test.java to (794)Primality_test.cpp

--- Practice/Below 1000/(794)Primality_test.cpp
+++ Practice/Below 1000/(794)Primality_test.cpp
@@ -1,38 +1,5 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-	    Scanner sc=new Scanner(System.in);
-		int t=sc.nextInt();
-		for(int i=0;i<t;i++)
-		{
-		    int n=sc.nextInt();
-		    int count=0;
-		    for(int j=2;j<=n;j++)
-		    {
-		        if(n%j==0)
-		        {
-		            count+=1;
-		        }
-		    }
-		    if(count==1)
-		    {
-		        System.out.println("yes");
-		    }
-		    else
-		    {
-		        System.out.println("no");
-		    }
-		}
-	}
-}
-
-
 In C++ Approach1
-/*
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -57,9 +24,10 @@ int main()
 	    isPrime(n) ? cout << "yes\n" :cout<<"no\n";
 	}
 }
-*/
+
+	
 Approach 2 in C++
-/*
+
 #include<iostream>
 using namespace std;
 int main()
@@ -81,4 +49,3 @@ int main()
         cout<<"no"<<endl;
     }
 }
-*/
