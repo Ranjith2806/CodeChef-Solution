ad988eb59d8fc74ee8a3b2baddec746ef0c55680
Rankumar <105041389+Ranjith2806@users.noreply.github.com>
2023-01-18 23:32:19 +0530

    Update and rename Interior_Design.java to Interior_Design.cpp
    Link to the problem
    https://www.codechef.com/submit/INTRDSGN?tab=statement

---
+++ Practice/Below 1000/Interior_Design.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,c,d;
+	    cin>>a>>b>>c>>d;
+	    cout<<(min((a+b),(c+d)))<<endl;
+	}
+	return 0;
+}
