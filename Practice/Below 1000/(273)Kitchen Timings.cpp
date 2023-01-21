f416b398d3a0ae69ce964c6869ad1a2fd6d37c17
Rankumar <105041389+Ranjith2806@users.noreply.github.com>
2023-01-21 23:37:11 +0530

    Kitchen Timings
    Link to problem
    https://www.codechef.com/problems/KITCHENTIME

---
+++ Practice/Below 1000/(273)Kitchen Timings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int test;
+	cin>>test;
+	while(test--)
+	{
+	    int num1,num2;
+	    cin>>num1>>num2;
+	    cout<<num2-num1<<endl;
+	}
+	return 0;
+}
